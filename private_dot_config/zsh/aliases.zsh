d350f358df58ac3ef55fd0a2d27d9ad79b2b5a7e
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-05-18 11:28:15 +0530

    Update .config/zsh/aliases.zsh Add .config/zsh/aliases/docker-compose.zsh Update .config/zsh/.zshrc

--- private_dot_config/zsh/aliases.zsh
+++ private_dot_config/zsh/aliases.zsh
@@ -62,6 +62,17 @@ function gunwipall() {
     fi
 }
 
+# Docker Compose
+alias dco='docker-compose'
+alias dcb='docker-compose build'
+alias dcr='docker-compose run'
+alias dcup='docker-compose up'
+alias dcupd='docker-compose up -d'
+alias dcupb='docker-compose up --build'
+alias dcupbd='docker-compose up --build -d'
+alias dcdown='docker-compose down'
+alias dce='docker-compose exec'
+
 # Global aliases
 alias -g B='|& bat'
 alias -g H='|& head'
