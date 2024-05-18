be7117be6f580dbcce8635a860848fdd2891e7b4
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-05-18 11:36:45 +0530

    Update .config/zsh/aliases.zsh Add .config/zsh/aliases/git.zsh Update .config/zsh/.zshrc

---
+++ private_dot_config/zsh/aliases/git.zsh
@@ -0,0 +1,24 @@
+alias gst='git status'
+alias ga='git add'
+alias gaa='git add -A'
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gb='git branch'
+alias gw='git switch'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gp='git pull'
+alias gP='git push'
+alias gl='git log'
+alias glo='git log --oneline'
+alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign --message "--wip-- [skip ci]"'
+alias gunwip='git rev-list --max-count=1 --format="%s" HEAD | grep -q "\--wip--" && git reset HEAD~1'
+# Similar to `gunwip` but recursive "Unwips" all recent `--wip--` commits not just the last one
+function gunwipall() {
+    local _commit=$(git log --grep='--wip--' --invert-grep --max-count=1 --format=format:%H)
+
+    # Check if a commit without "--wip--" was found and it's not the same as HEAD
+    if [[ "$_commit" != "$(git rev-parse HEAD)" ]]; then
+        git reset $_commit || return 1
+    fi
+}
