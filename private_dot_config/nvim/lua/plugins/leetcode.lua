e47a6ba06897acb65d3776ac8d0a942b7e374c78
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-05-20 16:46:21 +0530

    Update .config/nvim/lua/plugins/leetcode.lua

--- private_dot_config/nvim/lua/plugins/leetcode.lua
+++ private_dot_config/nvim/lua/plugins/leetcode.lua
@@ -1,27 +1,29 @@
-local leet_arg = "leetcode.nvim"
+local leet_arg = 'leetcode.nvim'
 
 return {
-  "kawre/leetcode.nvim",
-  build = ":TSUpdate html",
+  'kawre/leetcode.nvim',
+  build = ':TSUpdate html',
   dependencies = {
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "rcarriga/nvim-notify",
-    "nvim-tree/nvim-web-devicons",
+    'nvim-telescope/telescope.nvim',
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'rcarriga/nvim-notify',
+    'nvim-tree/nvim-web-devicons',
+    -- '3rd/image.nvim',
   },
   lazy = leet_arg ~= vim.fn.argv()[1],
   opts = {
     arg = leet_arg,
-    lang = "python3",
+    lang = 'python3',
     storage = {
-      home = "~/workspace/leetcode",
+      home = '~/workspace/leetcode',
     },
     injector = {
-      ["python3"] = {
-        before = true
+      ['python3'] = {
+        before = true,
       },
-    }
+    },
+    -- image_support = true,
   },
 }
