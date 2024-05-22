b365a13db975c7f82752034e4b69429852ba4b3e
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-05-22 13:26:45 +0530

    Update .config/nvim/lua/plugins/which-key.lua

--- private_dot_config/nvim/lua/plugins/which-key.lua
+++ private_dot_config/nvim/lua/plugins/which-key.lua
@@ -9,7 +9,7 @@ return {
     -- Document existing key chains
     require('which-key').register {
       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+      ['<leader>d'] = { name = '[D]ebug', _ = 'which_key_ignore' },
       ['<leader>n'] = { name = '[N]eoTree', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
