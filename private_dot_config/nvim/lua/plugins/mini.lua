6d620701cdd70371969ef57325039425890518cb
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-05-22 20:09:49 +0530

    Update .config/nvim/lua/core/keymaps.lua Update .config/nvim/lua/plugins/mini.lua

--- private_dot_config/nvim/lua/plugins/mini.lua
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -4,11 +4,22 @@ return {
     config = function()
       require('mini.ai').setup { n_lines = 500 }
 
-      require('mini.surround').setup()
-
       require('mini.align').setup()
 
       require('mini.animate').setup()
+
+      require('mini.basics').setup {
+        mappings = {
+          -- Prefix for mappings that toggle common options ('wrap', 'spell', ...).
+          option_toggle_prefix = '<leader>t',
+          -- Window navigation with <C-hjkl>, resize with <C-arrow>
+          windows = true,
+          -- Move cursor in Insert, Command, and Terminal mode with <M-hjkl>
+          move_with_alt = true,
+        },
+      }
+
+      require('mini.surround').setup()
     end,
   },
 }
