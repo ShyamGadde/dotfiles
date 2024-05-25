82257791cca0c0e5be70e5ffd907c781ed59c97f
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-05-26 04:42:31 +0530

    Update .config/nvim/lua/plugins/autopairs.lua

--- private_dot_config/nvim/lua/plugins/autopairs.lua
+++ private_dot_config/nvim/lua/plugins/autopairs.lua
@@ -1,7 +1,7 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  -- dependencies = { 'hrsh7th/nvim-cmp' },
+  dependencies = { 'hrsh7th/nvim-cmp' },
   opts = {
     check_ts = true, -- enable treesitter
     ts_config = {
@@ -9,11 +9,11 @@ return {
       javascript = { 'template_string' }, -- don't add pairs in javascript template_string
     },
   },
-  -- config = function(_, opts)
-  --   require('nvim-autopairs').setup(opts)
-  --   -- If you want to automatically add `(` after selecting a function or method
-  --   local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-  --   local cmp = require 'cmp'
-  --   cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-  -- end,
+  config = function(_, opts)
+    require('nvim-autopairs').setup(opts)
+    -- If you want to automatically add `(` after selecting a function or method
+    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+    local cmp = require 'cmp'
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+  end,
 }
