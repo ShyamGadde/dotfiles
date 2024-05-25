2eff8d6e5f785ca9c452481511d7b9b15fc92aea
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-05-25 18:24:33 +0530

    Update .config/nvim/lua/plugins/extras.lua Remove .config/nvim/lua/plugins/treesj.lua

--- private_dot_config/nvim/lua/plugins/extras.lua
+++ private_dot_config/nvim/lua/plugins/extras.lua
@@ -9,6 +9,29 @@ return {
     opts = {},
   },
 
+  {
+    'Wansmer/treesj',
+    keys = {
+      { 'gs', desc = 'Split/Join Toggle' },
+      { 'gS', desc = 'Split/Join Recursive Toggle' },
+    },
+    opts = {
+      use_default_keymaps = false,
+      max_join_length = 150,
+    },
+    config = function(_, opts)
+      local treesj = require 'treesj'
+      treesj.setup(opts)
+
+      -- For default preset
+      vim.keymap.set('n', 'gs', treesj.toggle)
+      -- For extending default preset with `recursive = true`
+      vim.keymap.set('n', 'gS', function()
+        treesj.toggle { split = { recursive = true } }
+      end)
+    end,
+  },
+
   -- {
   --   'szw/vim-maximizer',
   --   config = function()
