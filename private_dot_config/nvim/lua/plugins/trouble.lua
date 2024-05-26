b68f69de4e290ff67ed42a3abe8a2974dd4436c8
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-05-26 19:26:48 +0530

    Add .config/nvim/lua/plugins/trouble.lua

---
+++ private_dot_config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,9 @@
+return {
+  'folke/trouble.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'folke/todo-comments.nvim',
+  },
+  cmd = { 'TroubleToggle', 'Trouble' },
+  opts = { use_diagnostic_signs = true },
+}
