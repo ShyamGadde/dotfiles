02b70239fd4d1859a8651afc8bd6617069ea79f9
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-05-26 22:41:32 +0530

    Remove .config/nvim/lua/plugins/trouble.lua

--- private_dot_config/nvim/lua/plugins/trouble.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'folke/trouble.nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-    'folke/todo-comments.nvim',
-  },
-  cmd = { 'TroubleToggle', 'Trouble' },
-  opts = { use_diagnostic_signs = true },
-}
