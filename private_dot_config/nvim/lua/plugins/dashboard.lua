7619fcedbe403a2b91db1cb621abd4cec99aede2
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-06-02 19:59:46 +0530

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/dashboard.lua

--- private_dot_config/nvim/lua/plugins/dashboard.lua
+++ private_dot_config/nvim/lua/plugins/dashboard.lua
@@ -4,7 +4,6 @@ return {
   config = function()
     local alpha = require 'alpha'
     local dashboard = require 'alpha.themes.dashboard'
-    local fortune = require 'alpha.fortune'
 
     -- Set header
     dashboard.section.header.val = {
@@ -23,7 +22,7 @@ return {
     dashboard.section.buttons.val = {
       dashboard.button('e', '  New File', '<cmd> ene <BAR> startinsert <CR>'),
       dashboard.button('r', '  Restore workspace session', '<cmd> SessionRestore <CR>'),
-      dashboard.button('.', '  Recent files', ':Telescope oldfiles <CR>'),
+      dashboard.button('.', '  Recent files', '<cmd> Telescope oldfiles <CR>'),
       dashboard.button('f', '󰥨  Find File', '<cmd> Telescope find_files <CR>'),
       dashboard.button('g', '󰱼  Live Grep', '<cmd> Telescope live_grep <CR>'),
       dashboard.button('u', '  Update Plugins', '<cmd> Lazy update <CR>'),
@@ -38,8 +37,6 @@ return {
     dashboard.section.header.opts.hl = 'AlphaHeader'
     dashboard.section.buttons.opts.hl = 'AlphaButtons'
     dashboard.section.footer.opts.hl = 'AlphaFooter'
-
-    -- dashboard.opts.layout[1].val = 7
 
     -- Close Lazy and re-open when the dashboard is ready
     if vim.o.filetype == 'lazy' then
@@ -60,15 +57,13 @@ return {
       callback = function()
         local stats = require('lazy').stats()
         local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-        local footer = fortune()
+        -- local footer = fortune()
         local plugin_info = ' ' .. stats.loaded .. '/' .. stats.count .. ' plugins'
         local startup_time = ' ' .. ms .. 'ms'
         local v = vim.version()
         local nvim_version_info = ' ' .. 'v' .. v.major .. '.' .. v.minor .. '.' .. v.patch
-        local info_bar = '        ' .. nvim_version_info .. '  ' .. plugin_info .. '  ' .. startup_time
-        table.insert(footer, 1, info_bar)
-        table.insert(footer, 2, '')
-        dashboard.section.footer.val = footer
+        local info_bar = nvim_version_info .. '  ' .. plugin_info .. '  ' .. startup_time
+        dashboard.section.footer.val = info_bar
         pcall(vim.cmd.AlphaRedraw)
       end,
     })
