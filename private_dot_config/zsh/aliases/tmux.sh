c15741ab1db01209b8f12522b5127a90f3be6b80
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-06-03 21:07:57 +0530

    Handle ESC key in 'ta' function for tmux

--- private_dot_config/zsh/aliases/tmux.sh
+++ private_dot_config/zsh/aliases/tmux.sh
@@ -1,3 +1,7 @@
 ta() {
-	tmux attach -t "$(tmux ls | fzf --height 40% --layout=reverse --border | awk -F: '{print $1}')"
+	local session
+	session=$(tmux ls | fzf --height 40% --layout=reverse --border | awk -F: '{print $1}')
+	if [ -n "$session" ]; then
+		tmux attach -t "$session"
+	fi
 }
