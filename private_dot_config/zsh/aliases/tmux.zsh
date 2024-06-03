308d5966451ce2d59f204c9e3803f8d42c2abf10
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-06-03 21:12:11 +0530

    Change file extension

--- private_dot_config/zsh/aliases/tmux.zsh
+++ private_dot_config/zsh/aliases/tmux.zsh
@@ -1,7 +1,8 @@
-ta() {
+tmux_attach() {
 	local session
 	session=$(tmux ls | fzf --height 40% --layout=reverse --border | awk -F: '{print $1}')
 	if [ -n "$session" ]; then
 		tmux attach -t "$session"
 	fi
 }
+alias ta='tmux_attach'
