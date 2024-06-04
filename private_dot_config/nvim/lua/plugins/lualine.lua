7e37c4859e09f05fedb006125bd70d4167ff8462
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-06-04 14:45:18 +0530

    nvim: update lualine

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -8,6 +8,7 @@ return {
     require('lualine').setup {
       options = {
         theme = 'catppuccin',
+        globalstatus = true,
         -- component_separators = { left = '|', right = '|' },
         section_separators = { left = '', right = '' },
         -- section_separators = { left = '', right = '' },
