60ab76eb27b38716592cb908ea0adfc685e842b1
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-06-05 08:28:31 +0530

    nvim: update leetcode.nvim keymaps

--- private_dot_config/nvim/lua/plugins/leetcode.lua
+++ private_dot_config/nvim/lua/plugins/leetcode.lua
@@ -33,13 +33,13 @@ return {
       ['<leader>l'] = { name = '[L]eetCode', _ = 'which_key_ignore' },
     }
 
-    vim.keymap.set('n', '<leader>ld', '<cmd> Leet desc toggle <CR>', { desc = 'LeetCode toggle [d]escription' })
-    vim.keymap.set('n', '<leader>lm', '<cmd> Leet menu <CR>', { desc = 'LeetCode open [m]enu dashboard' })
-    vim.keymap.set('n', '<leader>li', '<cmd> Leet info <CR>', { desc = 'LeetCode [i]nformation about current questions' })
-    vim.keymap.set('n', '<leader>lr', '<cmd> Leet run <CR>', { desc = 'LeetCode [r]un solution' })
-    vim.keymap.set('n', '<leader>ls', '<cmd> Leet submit <CR>', { desc = 'LeetCode [s]ubmit solution' })
-    vim.keymap.set('n', '<leader>lS', '<cmd> Leet last_submit <CR>', { desc = 'LeetCode retrieve last [S]ubmitted code for the current question' })
-    vim.keymap.set('n', '<leader>lo', '<cmd> Leet open <CR>', { desc = 'LeetCode [o]pen current question in browser' })
-    vim.keymap.set('n', '<leader>lq', '<cmd> Leet exit <CR>', { desc = 'LeetCode [e]xit' })
+    vim.keymap.set('n', '<leader>ld', '<cmd> Leet desc toggle <CR>', { desc = 'Toggle [D]escription' })
+    vim.keymap.set('n', '<leader>lm', '<cmd> Leet menu <CR>', { desc = 'Open Leetcode [M]enu Dashboard' })
+    vim.keymap.set('n', '<leader>li', '<cmd> Leet info <CR>', { desc = 'Show [I]nformation about current question' })
+    vim.keymap.set('n', '<leader>lr', '<cmd> Leet run <CR>', { desc = '[R]un Solution' })
+    vim.keymap.set('n', '<leader>ls', '<cmd> Leet submit <CR>', { desc = '[S]ubmit Solution' })
+    vim.keymap.set('n', '<leader>lS', '<cmd> Leet last_submit <CR>', { desc = 'Retrieve last [S]ubmitted code for the current question' })
+    vim.keymap.set('n', '<leader>lo', '<cmd> Leet open <CR>', { desc = '[O]pen current question in browser' })
+    vim.keymap.set('n', '<leader>lq', '<cmd> Leet exit <CR>', { desc = '[Q]uit Leetcode' })
   end,
 }
