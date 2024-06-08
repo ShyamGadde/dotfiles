f5f8cc432c199ee2c70f5286fd6f9bc81280bb52
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-06-08 21:49:43 +0530

    Update Hyprland config

--- private_dot_config/hypr/scripts/executable_handle-monitor-connect.sh
+++ private_dot_config/hypr/scripts/executable_handle-monitor-connect.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 handle() {
-  case $1 in monitoradded*)
-    hyprctl dispatch moveworkspacetomonitor "1 0"
-    hyprctl dispatch moveworkspacetomonitor "2 0"
-    hyprctl dispatch moveworkspacetomonitor "3 0"
-    hyprctl dispatch moveworkspacetomonitor "4 0"
-    hyprctl dispatch moveworkspacetomonitor "5 0"
-    hyprctl dispatch moveworkspacetomonitor "6 1"
-    hyprctl dispatch moveworkspacetomonitor "7 1"
-    hyprctl dispatch moveworkspacetomonitor "8 1"
-    hyprctl dispatch moveworkspacetomonitor "9 1"
-    hyprctl dispatch moveworkspacetomonitor "10 1"
-  esac
+	case $1 in monitoradded*)
+		hyprctl dispatch moveworkspacetomonitor "1 1"
+		hyprctl dispatch moveworkspacetomonitor "2 1"
+		hyprctl dispatch moveworkspacetomonitor "3 1"
+		hyprctl dispatch moveworkspacetomonitor "4 1"
+		hyprctl dispatch moveworkspacetomonitor "5 1"
+		hyprctl dispatch moveworkspacetomonitor "6 1"
+		hyprctl dispatch moveworkspacetomonitor "7 1"
+		hyprctl dispatch moveworkspacetomonitor "8 0"
+		hyprctl dispatch moveworkspacetomonitor "9 1"
+		hyprctl dispatch moveworkspacetomonitor "10 0"
+		;;
+	esac
 }
 
 socat - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock" | while read -r line; do handle "$line"; done
