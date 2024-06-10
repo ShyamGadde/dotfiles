b65b8317481203ed682b3b799cb937471922be2b
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-06-10 17:57:15 +0530

    nvim: add vscode like insert line above/below behavior in insert mode

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,23 +1,23 @@
 --  See `:help keymap.set()`
-local keymap = vim.keymap
+local map = vim.keymap.set
 
 -- General keymaps
-vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true, desc = 'Make current file E[x]ecutable' })
+map('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true, desc = 'Make current file E[x]ecutable' })
 
 -- Clear highlight on pressing <Esc> in normal mode
-keymap.set('n', '<Esc>', '<cmd> nohlsearch <CR>')
+map('n', '<Esc>', '<cmd> nohlsearch <CR>')
 
 -- Disable space in normal and visual mode
-keymap.set({ 'n', 'v' }, '<Space>', '<Nop>')
+map({ 'n', 'v' }, '<Space>', '<Nop>')
 
 -- Delete single character without copying into register
-keymap.set('n', 'x', '"_x')
+map('n', 'x', '"_x')
 
 -- Diagnostic keymaps
-keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show line diagnostic [E]rror messages' })
-keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+map('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+map('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show line diagnostic [E]rror messages' })
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -25,6 +25,10 @@ keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnosti
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- VS Code like behaviour
+map('i', '<C-CR>', '<Esc>o', { desc = 'Insert Line Below' })
+map('i', '<C-S-CR>', '<Esc>O', { desc = 'Insert Line Above' })
 
 -- vim: ts=2 sts=2 sw=2 et
