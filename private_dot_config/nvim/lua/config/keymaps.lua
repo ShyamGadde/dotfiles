bcf1227128109a8fb696b6d6370b9a19647d80a2
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-06-15 13:09:00 +0530

    nvim: add custom config for LazyVim

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,51 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Use a command instead of a keymap
+vim.keymap.del("n", "<leader>L")
+vim.api.nvim_create_user_command("LazyVimChangelog", function()
+  LazyVim.news.changelog()
+end, { desc = "LazyVim Changelog" })
+
+vim.keymap.del("n", "<leader>l")
+map("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+-- toggle options
+map("n", "<leader>uf", function()
+  LazyVim.format.toggle(true)
+end, { desc = "Toggle Auto Format (Buffer)" })
+map("n", "<leader>uF", function()
+  LazyVim.format.toggle()
+end, { desc = "Toggle Auto Format (Global)" })
+
+-- lazygit
+map("n", "<leader>gg", function()
+  LazyVim.lazygit()
+end, { desc = "Lazygit (cwd)" })
+map("n", "<leader>gG", function()
+  LazyVim.lazygit({ cwd = LazyVim.root.git() })
+end, { desc = "Lazygit (Root Dir)" })
+
+map("n", "<leader>gl", function()
+  LazyVim.lazygit({ args = { "log" } })
+end, { desc = "Lazygit Log (cwd)" })
+map("n", "<leader>gL", function()
+  LazyVim.lazygit({ args = { "log" }, cwd = LazyVim.root.git() })
+end, { desc = "Lazygit Log" })
+
+-- floating terminal
+local lazyterm = function()
+  LazyVim.terminal(nil, { cwd = LazyVim.root() })
+end
+map("n", "<leader>ft", function()
+  LazyVim.terminal()
+end, { desc = "Terminal (cwd)" })
+map("n", "<leader>fT", lazyterm, { desc = "Terminal (Root Dir)" })
+
+-- Delete single character without copying into register
+map("n", "x", '"_x')
+
+map("n", "<leader>fx", "<cmd>!chmod +x %<CR>", { desc = "Make Current File Executable" })
--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,23 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+vim.g.lazyvim_picker = "telescope"
+
+opt.clipboard = ""
+opt.inccommand = "split"
+
+-- Configure tabs and spaces
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+
+vim.diagnostic.config({
+  float = { border = "rounded" }, -- add border to diagnostic popups
+})
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+opt.formatoptions = "jcrqlnt"
+
+opt.breakindent = true
