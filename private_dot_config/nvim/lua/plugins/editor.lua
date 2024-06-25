9529ea382dac514ba5f9e73d70c046724f3c9d9c
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-06-25 17:22:26 +0530

    nvim: update neo-tree config

--- private_dot_config/nvim/lua/plugins/editor.lua
+++ private_dot_config/nvim/lua/plugins/editor.lua
@@ -1,4 +1,33 @@
 return {
+  -- File Explorer
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+      "3rd/image.nvim",
+    },
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+      { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+      { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (Root Dir)", remap = true },
+    },
+    opts = {
+      enable_cursor_hijack = true, -- If enabled neotree will keep the cursor on the first letter of the filename when moving in the tree.
+      use_popups_for_input = false,
+    },
+  },
   {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
