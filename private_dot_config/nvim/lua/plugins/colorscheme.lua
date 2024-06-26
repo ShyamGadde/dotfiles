3ee93bfebde59410abbf11743bda3633e4c42991
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-06-26 21:56:03 +0530

    nvim: configure rainbow delimiters and indent lines

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -8,6 +8,13 @@ return {
           GitSignsChange = { fg = colors.blue }, -- Can't tell the difference between yellow and green, me colorblind.
         }
       end,
+      integrations = {
+        indent_blankline = {
+          enabled = true,
+          colored_indent_levels = true,
+        },
+        rainbow_delimiters = true,
+      },
     },
   },
 }
