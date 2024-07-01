b67d756c08ae5e8acd2dc6340a4f397f8043900d
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-07-01 12:24:35 +0530

    hyprland: update monitor configuration

--- private_dot_config/hypr/scripts/executable_handle-monitor-connect.sh
+++ private_dot_config/hypr/scripts/executable_handle-monitor-connect.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 handle() {
-	echo "$1"
-	case $1 in
-	monitoraddedv2*)
-		hyprctl dispatch moveworkspacetomonitor "1 1"
-		hyprctl dispatch moveworkspacetomonitor "2 1"
-		hyprctl dispatch moveworkspacetomonitor "3 1"
-		hyprctl dispatch moveworkspacetomonitor "4 1"
-		hyprctl dispatch moveworkspacetomonitor "5 1"
-		hyprctl dispatch moveworkspacetomonitor "6 1"
-		hyprctl dispatch moveworkspacetomonitor "7 1"
-		hyprctl dispatch moveworkspacetomonitor "8 1"
-		hyprctl dispatch moveworkspacetomonitor "9 1"
-		hyprctl dispatch moveworkspacetomonitor "10 0"
+    case $1 in
+        monitoraddedv2*)
+            hyprctl dispatch moveworkspacetomonitor "1 1"
+            hyprctl dispatch moveworkspacetomonitor "2 1"
+            hyprctl dispatch moveworkspacetomonitor "3 1"
+            hyprctl dispatch moveworkspacetomonitor "4 1"
+            hyprctl dispatch moveworkspacetomonitor "5 1"
+            hyprctl dispatch moveworkspacetomonitor "6 1"
+            hyprctl dispatch moveworkspacetomonitor "7 1"
+            hyprctl dispatch moveworkspacetomonitor "8 1"
+            hyprctl dispatch moveworkspacetomonitor "9 1"
+            hyprctl dispatch moveworkspacetomonitor "10 1"
 
-		brightnessctl -s set 0
-		;;
-	monitorremoved*)
-		brightnessctl -r
-		;;
-	esac
+            brightnessctl -s set 0
+            ;;
+        monitorremoved*)
+            hyprctl dispatch focusurgentorlast
+            brightnessctl -r
+            ;;
+    esac
 }
 
 socat - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock" | while read -r line; do handle "$line"; done
