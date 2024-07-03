d1adad8bedf45aff748c18d652044464226fdec5
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-07-03 14:48:04 +0530

    nvim: add the refactoring.nvim plugin

--- private_dot_config/nvim/lua/config/lazy.lua
+++ private_dot_config/nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.dial" },
+    { import = "lazyvim.plugins.extras.editor.refactoring" },
     -- { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "plugins" },
   },
