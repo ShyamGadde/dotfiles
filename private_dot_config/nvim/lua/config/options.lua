12a1ed7070ce374ac8d34b1403fdde6b8f2ef5f9
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-07-03 14:48:37 +0530

    nvim: configure the root_spec to prioritize .git

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -2,27 +2,30 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
-
 vim.g.lazyvim_picker = "telescope"
+vim.g.root_spec = { { ".git", "lua" }, "lsp", "cwd" }
+
+local opt = vim.opt
 
 opt.clipboard = ""
 opt.inccommand = "split"
+opt.breakindent = true
 
 -- Configure tabs and spaces
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.softtabstop = 4
 
+-- Appearance
 vim.diagnostic.config({
   float = { border = "rounded" }, -- Add border to diagnostic popups
 })
 opt.pumblend = 0 -- Popup blend
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- FIX: These options don't seem to be set for some reason
-opt.formatoptions = "jcrqlnt"
+-- opt.formatoptions = "jcrqlnt"
 
-opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-opt.breakindent = true
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
 
 vim.opt.diffopt:append("linematch:50")
