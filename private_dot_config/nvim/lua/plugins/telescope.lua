c7ee0f35dcc9b3764c07ebcc6d8773b6f3f808fc
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-07-06 16:11:28 +0530

    nvim: update config for telescope-undo

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -109,11 +109,11 @@ return {
     opts = {
       extensions = {
         undo = {
-          side_by_side = true,
-          layout_strategy = "vertical",
-          layout_config = {
-            preview_height = 0.6,
-          },
+          -- side_by_side = true,
+          -- layout_strategy = "vertical",
+          -- layout_config = {
+          --   preview_height = 0.5,
+          -- },
         },
       },
     },
