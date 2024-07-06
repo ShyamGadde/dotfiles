71216212837d153039680006454ac7d1855f443b
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-07-06 14:48:09 +0530

    nvim: move disabled plugins into their own file

---
+++ private_dot_config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "echasnovski/mini.pairs",
+    enabled = false,
+    -- INFO: Why?
+    -- When trying to insert an ending quote results in a pair.
+    -- E.g., `"hello|` -> insert `"` -> `"hello"|"`
+    -- Couldn't figure out how to fix this, hence disabled and replaced with "nvim-autopairs".
+    -- NOTE: This behaviour was only noticed in Python files.
+  },
+}
