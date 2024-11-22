a1b6f65ebb6178396abe9d7c1db2aa1e0680aa31
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-11-22 13:07:53 +0530

    nvim: update mech to find phpcs config file to use for current file

--- private_dot_config/nvim/lua/plugins/php.lua
+++ private_dot_config/nvim/lua/plugins/php.lua
@@ -60,16 +60,53 @@ return {
       linters_by_ft = {
         php = { "phpcs" },
       },
-      -- linters = {
-      --   phpcs = {
-      --     args = {
-      --       "-q",
-      --       "--standard=WordPress",
-      --       "--report=json",
-      --       "-",
-      --     },
-      --   },
-      -- },
+      linters = {
+        phpcs = {
+          args = {
+            "--report=json",
+            "-q",
+            "-s",
+            function()
+              local current_file_dir = vim.fn.expand("%:p:h")
+              local cwd = vim.fn.getcwd() -- The directory where Neovim was opened
+              local config_files = {
+                ".phpcs.xml",
+                "phpcs.xml",
+                ".phpcs.xml.dist",
+                "phpcs.xml.dist",
+                "phpcs.ruleset.xml",
+                "ruleset.xml",
+              }
+
+              -- Function to search for config files starting from the provided directory
+              local function find_config_file(start_dir)
+                for _, filename in ipairs(config_files) do
+                  local config_file = vim.fn.findfile(filename, start_dir)
+                  if config_file and #config_file > 0 then
+                    return config_file
+                  end
+                end
+                return nil
+              end
+
+              -- Start searching from the current file's directory and move upwards
+              local config_file = find_config_file(current_file_dir)
+              while not config_file and current_file_dir ~= cwd do
+                -- If no config file is found, go up one level
+                current_file_dir = vim.fn.fnamemodify(current_file_dir, ":h")
+                config_file = find_config_file(current_file_dir)
+              end
+
+              if config_file then
+                return "--standard=" .. vim.fn.fnameescape(config_file)
+              else
+                return nil -- If no config file is found, return nil to omit this argument
+              end
+            end,
+            "-",
+          },
+        },
+      },
     },
   },
 
