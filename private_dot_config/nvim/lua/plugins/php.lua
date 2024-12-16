62e23d56a47a72935d0120ca0578546d49a7599b
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2024-12-16 16:17:08 +0530

    nvim: fix phpcbf not picking up standards file inside a subdir for files in that subdir
    Signed-off-by: Shyamsundar Gadde <shyamgadde1602@gmail.com>

--- private_dot_config/nvim/lua/plugins/php.lua
+++ private_dot_config/nvim/lua/plugins/php.lua
@@ -1,3 +1,47 @@
+-- Helper function to find the closest PHP CodeSniffer standards file
+local function find_phpcs_standard()
+  local current_file_dir = vim.fn.expand("%:p:h")
+  local cwd = vim.fn.getcwd() -- The directory where Neovim was opened
+  local config_files = {
+    ".phpcs.xml",
+    "phpcs.xml",
+    ".phpcs.xml.dist",
+    "phpcs.xml.dist",
+    "phpcs.ruleset.xml",
+    "ruleset.xml",
+  }
+
+  -- Function to search for config files starting from the provided directory
+  local function search_config_file(start_dir)
+    for _, filename in ipairs(config_files) do
+      local config_file = vim.fn.findfile(filename, start_dir)
+      if config_file and #config_file > 0 then
+        return config_file
+      end
+    end
+    return nil
+  end
+
+  -- Start searching from the current file's directory and move upwards
+  local config_file = search_config_file(current_file_dir)
+
+  -- Stop searching one level below the current working directory (cwd)
+  -- because if the config file exists in the current directory or above,
+  -- phpcs/phpcbf will handle it internally.
+  while not config_file and current_file_dir ~= cwd do
+    -- Move up one directory level
+    current_file_dir = vim.fn.fnamemodify(current_file_dir, ":h")
+    config_file = search_config_file(current_file_dir)
+  end
+
+  if config_file then
+    return "--standard=" .. vim.fn.fnameescape(config_file)
+  else
+    -- return "--standard=WordPress"
+    return nil -- If no config file is found, return nil to omit this argument
+  end
+end
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -71,43 +115,7 @@ return {
             "--report=json",
             "-q",
             "-s",
-            function()
-              local current_file_dir = vim.fn.expand("%:p:h")
-              local cwd = vim.fn.getcwd() -- The directory where Neovim was opened
-              local config_files = {
-                ".phpcs.xml",
-                "phpcs.xml",
-                ".phpcs.xml.dist",
-                "phpcs.xml.dist",
-                "phpcs.ruleset.xml",
-                "ruleset.xml",
-              }
-
-              -- Function to search for config files starting from the provided directory
-              local function find_config_file(start_dir)
-                for _, filename in ipairs(config_files) do
-                  local config_file = vim.fn.findfile(filename, start_dir)
-                  if config_file and #config_file > 0 then
-                    return config_file
-                  end
-                end
-                return nil
-              end
-
-              -- Start searching from the current file's directory and move upwards
-              local config_file = find_config_file(current_file_dir)
-              while not config_file and current_file_dir ~= cwd do
-                -- If no config file is found, go up one level
-                current_file_dir = vim.fn.fnamemodify(current_file_dir, ":h")
-                config_file = find_config_file(current_file_dir)
-              end
-
-              if config_file then
-                return "--standard=" .. vim.fn.fnameescape(config_file)
-              else
-                return nil -- If no config file is found, return nil to omit this argument
-              end
-            end,
+            find_phpcs_standard,
             "-",
           },
         },
@@ -121,11 +129,13 @@ return {
       formatters_by_ft = {
         php = { "phpcbf", "trim_whitespace", "trim_newlines" },
       },
-      -- formatters = {
-      --   phpcbf = {
-      --     prepend_args = { "--standard=WordPress" },
-      --   },
-      -- },
+      formatters = {
+        phpcbf = {
+          prepend_args = function()
+            return { find_phpcs_standard() }
+          end,
+        },
+      },
     },
   },
 }
