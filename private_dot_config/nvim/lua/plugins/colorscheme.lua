08a11e51a6e72f77e067360dc60f2db48467035b
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2024-12-16 21:14:54 +0530

    nvim: add style config for catppuccin colorscheme
    Signed-off-by: Shyamsundar Gadde <shyamgadde1602@gmail.com>

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -8,15 +8,17 @@ return {
           GitSignsChange = { fg = colors.blue }, -- Can't tell the difference between yellow and green, me colorblind. Hence, me override.
         }
       end,
+      dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+      flavor = "mocha",
       integrations = {
         native_lsp = {
           enabled = true,
-          virtual_text = {
-            errors = { "italic" },
-            hints = { "italic" },
-            warnings = { "italic" },
-            information = { "italic" },
-            ok = { "italic" },
+          inlay_hints = {
+            background = true,
           },
           underlines = {
             errors = { "undercurl" },
@@ -24,8 +26,12 @@ return {
             warnings = { "undercurl" },
             information = { "undercurl" },
           },
-          inlay_hints = {
-            background = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+            ok = { "italic" },
           },
         },
         nvim_surround = true,
@@ -36,6 +42,22 @@ return {
           -- style = "nvchad",
         },
       },
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {},  -- Uncomment to turn off hardcoded styles.
+      },
+      transparent_background = false,
     },
   },
 
