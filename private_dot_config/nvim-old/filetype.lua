3b87044bfea7826d4c8d4adbea09c9f0db5fe135
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2024-12-16 22:01:14 +0530

    chore: remove nvim-old
    Signed-off-by: Shyamsundar Gadde <shyamgadde1602@gmail.com>

--- private_dot_config/nvim-old/filetype.lua
+++
@@ -1,7 +0,0 @@
-vim.filetype.add {
-  pattern = {
-    ['.*%.hl'] = 'hyprlang',
-    ['hypr.*%.conf'] = 'hyprlang',
-    ['.*/hypr/.*%.conf'] = 'hyprlang',
-  },
-}
--- private_dot_config/nvim-old/lua/config/autocmds.lua
+++
@@ -1,3 +0,0 @@
---  See `:help lua-guide-autocommands`
-
--- vim: ts=2 sts=2 sw=2 et
