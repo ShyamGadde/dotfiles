17dbf6e2de39562bdab9287a9fcfd4c8a730b2ce
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2024-12-18 17:00:29 +0530

    scripts: remove gh-notify from gh-cli extension list
    Signed-off-by: Shyamsundar Gadde <shyamgadde1602@gmail.com>

--- run_once_after_00-setup-gh-cli.sh
+++ run_once_after_00-setup-gh-cli.sh
@@ -5,6 +5,5 @@ gh auth login
 # Download extensions:
 gh extension install github/gh-copilot
 gh extension install yuler/gh-download
-gh extension install meiji163/gh-notify
 
 echo "**Run 'ssh -T git@github.com'**"
