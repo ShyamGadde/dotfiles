182f3386623033e9232b0a9007f96549479b537e
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2024-12-23 14:27:01 +0530

    zsh: remove if condition check that was slowing down alias loading
    Signed-off-by: Shyamsundar Gadde <shyamgadde1602@gmail.com>

--- private_dot_config/zsh/aliases/gh-copilot-aliases.zsh
+++ private_dot_config/zsh/aliases/gh-copilot-aliases.zsh
@@ -1,20 +1,18 @@
 # shellcheck shell=bash
 
-if gh extension list | grep -q 'github/gh-copilot'; then
-    function copilot_shell_suggest() {
-        gh copilot suggest -t shell "$@"
-    }
-    alias '??'='copilot_shell_suggest'
+function copilot_shell_suggest() {
+    gh copilot suggest -t shell "$@"
+}
+alias '??'='copilot_shell_suggest'
 
-    # Function to handle Git command suggestions
-    function copilot_git_suggest() {
-        gh copilot suggest -t git "$@"
-    }
-    alias 'git?'='copilot_git_suggest'
+# Function to handle Git command suggestions
+function copilot_git_suggest() {
+    gh copilot suggest -t git "$@"
+}
+alias 'git?'='copilot_git_suggest'
 
-    # Function to handle GitHub CLI command suggestions
-    function copilot_gh_suggest() {
-        gh copilot suggest -t gh "$@"
-    }
-    alias 'gh?'='copilot_gh_suggest'
-fi
+# Function to handle GitHub CLI command suggestions
+function copilot_gh_suggest() {
+    gh copilot suggest -t gh "$@"
+}
+alias 'gh?'='copilot_gh_suggest'
