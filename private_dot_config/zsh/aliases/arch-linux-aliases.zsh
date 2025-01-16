832f54be2de08c863029b116bd3e27f44ef01457
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2025-01-16 08:41:17 +0530

    zsh: add function to update everything that can be
    Signed-off-by: Shyamsundar Gadde <shyamgadde1602@gmail.com>

--- private_dot_config/zsh/aliases/arch-linux-aliases.zsh
+++ private_dot_config/zsh/aliases/arch-linux-aliases.zsh
@@ -13,3 +13,32 @@ alias big="expac -H M '%m\t%n' | sort -h | nl"                                # 
 [ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru'
 
 alias update-mirrors="sudo reflector --verbose --protocol https --age 24 --fastest 15 --save /etc/pacman.d/mirrorlist && bat /etc/pacman.d/mirrorlist" # Get the fastest mirrors
+
+function update() {
+    local BLUE="$(tput setaf 39)"
+    local RED="$(tput setaf 1)"
+    local RESET="$(tput sgr0)"
+
+    echo -e "\n${BLUE}Starting system updates...${RESET}\n"
+
+    echo -e "${BLUE}Updating Pacman packages...${RESET}\n"
+    if ! sudo pacman -Syu; then
+        echo -e "\n${RED}Pacman update failed.${RESET}\n"
+    fi
+
+    echo -e "\n${BLUE}Updating AUR packages...${RESET}\n"
+    if ! paru -Sua; then
+        echo -e "\n${RED}AUR update failed.${RESET}\n"
+    fi
+
+    # Update Zinit (self, plugins and snippets)
+    echo -e "\n${BLUE}Updating Zinit...${RESET}\n"
+    if ! zinit update --all; then
+        echo -e "\n${RED}Zinit update failed.${RESET}\n"
+    fi
+
+    echo -e "\n${BLUE}Updating GitHub CLI extensions...${RESET}\n"
+    if ! gh extension upgrade --all; then
+        echo -e "\n${RED}GitHub CLI extensions update failed.${RESET}\n"
+    fi
+}
